d0961d837af0ea0feb13cf4a565579b55dbefe43
Danil1239 <borzij555@gmail.com>
2021-10-28 14:01:18 +0300

    Day 4

---
+++ src/components/StudentInfo.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+    {{student.name}}
+    {{student.group}}
+    <img v-bind:src="student.photo">
+</div>
+</template>
+<script>
+import Vue from 'vue'
+import axios from 'axios'
+
+export default {
+    props: {
+           id: '',
+       },
+data : function(){
+    return{
+        student: {},
+    };
+},
+mounted: function(){
+axios.get("http://46.101.212.195:3000/students/"+this.id).then((response) => {
+           console.log(response.data)
+           this.student = response.data;
+        })
+},
+}
+</script>
